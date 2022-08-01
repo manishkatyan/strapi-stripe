e88b087911585bdd8dafdc0718fd4880f9851c56
nishekh-e-r <nishekh@higheredlab.com>
2022-08-01 16:07:33 +0530

    fix: resloved static folder path error

--- server/middlewares/staticFiles.js
+++ server/middlewares/staticFiles.js
@@ -1,18 +1,24 @@
 /* eslint-disable node/no-missing-require */
 
-'use strict';
+"use strict";
 
-const path = require('path');
-const koaStatic = require('koa-static');
+const path = require("path");
+const koaStatic = require("koa-static");
 
 module.exports = async ({ strapi }) => {
   strapi.server.routes([
     {
-      method: 'GET',
-      path: '/plugins/strapi-stripe/static/(.*)',
+      method: "GET",
+      path: "/plugins/strapi-stripe/static/(.*)",
       async handler(ctx, next) {
         ctx.url = path.basename(`${ctx.url}/stripe.js`);
-        const staticFolder = path.resolve(strapi.dirs.extensions, 'strapi-stripe', 'public');
+        const folderPath =
+          strapi.dirs.extensions || strapi.dirs.dist.extensions;
+        const staticFolder = path.resolve(
+          folderPath,
+          "strapi-stripe",
+          "public"
+        );
         return koaStatic(staticFolder)(ctx, next);
       },
       config: {
