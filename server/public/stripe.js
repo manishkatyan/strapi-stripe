321cf5eec171ff6ddc8ce09122f5e8871af71e18
nishekh-e-r <nishekh@higheredlab.com>
2022-08-03 16:43:32 +0530

    fix:lint error

--- server/public/stripe.js
+++ server/public/stripe.js
@@ -1,22 +1,19 @@
 /* eslint-disable no-undef */
-"use strict";
+'use strict';
 
 window.onload = () => {
   // for product Checkout
-  const ssProduct = document.querySelectorAll(".SS_ProductCheckout");
+  const ssProduct = document.querySelectorAll('.SS_ProductCheckout');
   if (ssProduct) {
-    ssProduct.forEach((product) => {
-      product.addEventListener("click", function handleClick(event) {
-        SS_ProductCheckout(
-          event.target?.dataset?.id,
-          event.target?.dataset?.url
-        );
+    ssProduct.forEach(product => {
+      product.addEventListener('click', function handleClick(event) {
+        SS_ProductCheckout(event.target.dataset.id, event.target.dataset.url);
       });
     });
   }
   // for storing product payment order in strapi
   const params = new URLSearchParams(document.location.search);
-  const checkoutSessionId = params.get("sessionId");
+  const checkoutSessionId = params.get('sessionId');
   if (checkoutSessionId) {
     SS_GetProductPaymentDetails(checkoutSessionId);
   }
@@ -25,21 +22,21 @@ window.onload = () => {
 // product Checkout logic
 
 function SS_ProductCheckout(productId, baseUrl) {
-  localStorage.setItem("strapiStripeUrl", baseUrl);
-  const getProductApi = baseUrl + "/strapi-stripe/getProduct/" + productId;
-  const checkoutSessionUrl = baseUrl + "/strapi-stripe/createCheckoutSession/";
+  localStorage.setItem('strapiStripeUrl', baseUrl);
+  const getProductApi = baseUrl + '/strapi-stripe/getProduct/' + productId;
+  const checkoutSessionUrl = baseUrl + '/strapi-stripe/createCheckoutSession/';
 
   fetch(getProductApi, {
-    method: "get",
-    mode: "cors",
+    method: 'get',
+    mode: 'cors',
     headers: new Headers({
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     }),
   })
-    .then((response) => response.json())
-    .then((response) => {
+    .then(response => response.json())
+    .then(response => {
       fetch(checkoutSessionUrl, {
-        method: "post",
+        method: 'post',
         body: JSON.stringify({
           stripePriceId: response.stripePriceId,
           stripePlanId: response.stripePlanId,
@@ -47,13 +44,13 @@ function SS_ProductCheckout(productId, baseUrl) {
           productId: response.id,
           productName: response.title,
         }),
-        mode: "cors",
+        mode: 'cors',
         headers: new Headers({
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         }),
       })
-        .then((response) => response.json())
-        .then((response) => {
+        .then(response => response.json())
+        .then(response => {
           if (response.id) {
             window.location.replace(response.url);
           }
@@ -64,31 +61,31 @@ function SS_ProductCheckout(productId, baseUrl) {
 //  storing product payment order in strapi logic
 
 function SS_GetProductPaymentDetails(checkoutSessionId) {
-  const baseUrl = localStorage.getItem("strapiStripeUrl");
+  const baseUrl = localStorage.getItem('strapiStripeUrl');
   const retrieveCheckoutSessionUrl =
-    baseUrl + "/strapi-stripe/retrieveCheckoutSession/" + checkoutSessionId;
+    baseUrl + '/strapi-stripe/retrieveCheckoutSession/' + checkoutSessionId;
   fetch(retrieveCheckoutSessionUrl, {
-    method: "get",
-    mode: "cors",
+    method: 'get',
+    mode: 'cors',
     headers: new Headers({
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     }),
   })
-    .then((response) => response.json())
-    .then((response) => {
-      if (response.payment_status === "paid") {
+    .then(response => response.json())
+    .then(response => {
+      if (response.payment_status === 'paid') {
         if (
           window.performance
-            .getEntriesByType("navigation")
-            .map((nav) => nav.type)
-            .includes("reload")
+            .getEntriesByType('navigation')
+            .map(nav => nav.type)
+            .includes('reload')
         ) {
-          console.info("website reloded");
+          console.info('website reloded');
         } else {
           // store payment in strapi
-          const stripePaymentUrl = baseUrl + "/strapi-stripe/stripePayment";
+          const stripePaymentUrl = baseUrl + '/strapi-stripe/stripePayment';
           fetch(stripePaymentUrl, {
-            method: "post",
+            method: 'post',
             body: JSON.stringify({
               txnDate: new Date(),
               transactionId: response.id,
@@ -99,9 +96,9 @@ function SS_GetProductPaymentDetails(checkoutSessionId) {
               customerEmail: response.customer_details.email,
               stripeProduct: response.metadata.productId,
             }),
-            mode: "cors",
+            mode: 'cors',
             headers: new Headers({
-              "Content-Type": "application/json",
+              'Content-Type': 'application/json',
             }),
           });
         }
