2bfb01ae192798de43d680d8d660e3c104e48b7c
nishekh-e-r <nishekh@higheredlab.com>
2022-11-15 14:34:15 +0530

    fix:lint

--- server/middlewares/staticFiles.js
+++ server/middlewares/staticFiles.js
@@ -1,24 +1,20 @@
+/* eslint-disable node/no-extraneous-require */
 /* eslint-disable node/no-missing-require */
 
-"use strict";
+'use strict';
 
-const path = require("path");
-const koaStatic = require("koa-static");
+const path = require('path');
+const koaStatic = require('koa-static');
 
 module.exports = async ({ strapi }) => {
   strapi.server.routes([
     {
-      method: "GET",
-      path: "/plugins/strapi-stripe/static/(.*)",
+      method: 'GET',
+      path: '/plugins/strapi-stripe/static/(.*)',
       async handler(ctx, next) {
         ctx.url = path.basename(`${ctx.url}/stripe.js`);
-        const folderPath =
-          strapi.dirs.extensions || strapi.dirs.dist.extensions;
-        const staticFolder = path.resolve(
-          folderPath,
-          "strapi-stripe",
-          "public"
-        );
+        const folderPath = strapi.dirs.extensions || strapi.dirs.dist.extensions;
+        const staticFolder = path.resolve(folderPath, 'strapi-stripe', 'public');
         return koaStatic(staticFolder)(ctx, next);
       },
       config: {
